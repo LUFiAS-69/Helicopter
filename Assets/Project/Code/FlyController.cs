56e684debcd7027dcf8030836b60e703fa20c68e
L U F i A S <lufias4november@gmail.com>
2023-08-19 17:53:41 +0600

    Level upgrade and Rigidbody Added

--- Assets/Project/Code/FlyController.cs
+++ Assets/Project/Code/FlyController.cs
@@ -5,31 +5,36 @@ public class FlyController : MonoBehaviour
 {
     [SerializeField] float flySpeed;
     [SerializeField] float steerAmount;
+    [SerializeField] float gravity;
     [SerializeField] Transform body;
 
     float rollMagnitude = 20;
     float pitchMagnitude = 30;
-    float steer;
+    float steer, pitch, roll;
+    float horizontalInput, verticalInput;
 
     private void Update()
     {
         //input
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticallInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
         //smooth
         steer += horizontalInput * steerAmount * Time.deltaTime;
-        float pitch = Mathf.Lerp(0, rollMagnitude, Mathf.Abs(verticallInput)) * Mathf.Sign(verticallInput);
-        float roll = Mathf.Lerp(0, pitchMagnitude, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
+        pitch = Mathf.Lerp(0, rollMagnitude, Mathf.Abs(verticalInput)) * Mathf.Sign(verticalInput);
+        roll = Mathf.Lerp(0, pitchMagnitude, Mathf.Abs(horizontalInput)) * -Mathf.Sign(horizontalInput);
 
         //movement
-        transform.position += transform.forward * verticallInput * flySpeed * Time.deltaTime;
+        transform.position += transform.forward * verticalInput * flySpeed * Time.deltaTime;
         transform.localRotation = Quaternion.Euler(Vector3.up * steer + Vector3.forward * roll);
         body.localRotation = Quaternion.Euler(Vector3.right * pitch);
 
         //up-down
-        if (Input.GetKey(KeyCode.E)) transform.Translate(Vector3.up * (flySpeed / 3) * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.Q)) transform.Translate(Vector3.down * (flySpeed / 3) * Time.deltaTime);
+        if (Input.GetKey(KeyCode.E)) transform.Translate(Vector3.up * (flySpeed / 5) * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.Q)) transform.Translate(Vector3.down * (flySpeed / 5) * Time.deltaTime);
+
+        //gravity
+        else transform.Translate(Vector3.down * (gravity) * Time.deltaTime);
     }
 
 }
